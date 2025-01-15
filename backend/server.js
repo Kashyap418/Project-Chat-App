1841390debecb4239482233cb90b94c61990d5cd
Kashyap Raina <kashyapraina418@gmail.com>
2025-01-15 21:38:05 +0530

    1st part completed -backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+// const express=require('express');
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+dotenv.config();
+
+const PORT=process.env.PORT || 5000
+
+const app=express();
+
+app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body) 
+app.use(cookieParser());
+
+app.use('/api/auth',authRoutes); 
+app.use('/api/messages',messageRoutes);  
+app.use('/api/users',userRoutes);  
+
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`Server Running on Port: ${PORT}`);
+})
---
+++ notes.js
@@ -0,0 +1,4 @@
+// When you add --save-dev, the package is installed and listed under the "devDependencies"
+// section in your package.json file.
+// Development dependencies are tools or libraries that are only needed during development and
+// not required in the production environment.
