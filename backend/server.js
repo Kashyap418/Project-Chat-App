2487275ee20f51b9380231e7ad2e13cd2f8cdfa9
Kashyap Raina <kashyapraina418@gmail.com>
2025-01-18 14:32:11 +0530

    chat app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-// const express=require('express');
+// const express=require('express');  
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,10 +11,11 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-dotenv.config();
-
 const PORT=process.env.PORT || 5000
 
+dotenv.config();
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body) 
 app.use(cookieParser());
@@ -22,6 +24,11 @@ app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);  
 app.use('/api/users',userRoutes);  
 
+app.use(express.static(path.join(__dirname, '/frontend/dist'))); 
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 server.listen(PORT,()=>{
     connectToMongoDB();
